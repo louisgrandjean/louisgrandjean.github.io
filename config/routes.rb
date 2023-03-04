113f6974801e56b863f41b479ba29654eb12c190
Louis Grandjean <71328072+louisgrandjean@users.noreply.github.com>
2023-03-04 12:13:05 +0100

    first

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :messages
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "messages#index"
+end
